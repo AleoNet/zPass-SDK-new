358da0b8312360617df9a9245cef29b87e3b750b
Zk <zklim@aleo.org>
2024-12-07 20:58:25 +0800

    add createAleoWorker helper & update deps

--- src/core/sdk.ts
+++ src/core/sdk.ts
@@ -13,6 +13,7 @@ import {
     RecordPlaintext,
     ViewKey,
     OfflineQuery,
+    createAleoWorker,
 } from '@provablehq/sdk/mainnet.js';
 import * as wasm from 'zpass-credential-signer';
 import { SDKError } from '../errors';
@@ -27,7 +28,7 @@ import {
 import type { TransactionModel, Output } from '@provablehq/sdk/mainnet.js';
 
 export type { TransactionModel, Output };
-export { OfflineQuery, Account };
+export { OfflineQuery, Account, createAleoWorker };
 
 export class ZPassSDK {
     private programManager: ProgramManager;
@@ -217,11 +218,6 @@ export class ZPassSDK {
             functionName
         );
         return res;
-    }
-
-    public async getRecords() {
-        // Not implemented yet
-        throw new SDKError("Not implemented yet");
     }
 
     async onChainInteract(options: ProveOnChainOptions): Promise<string> {
--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,5 @@ export { HashAlgorithm } from './interfaces';
 export { SDKError } from './errors';
 export type { TransactionModel, Output } from './core/sdk';
 export { OfflineQuery, Account } from './core/sdk';
+export { createAleoWorker } from './core/createAleoWorker';
 export { ZPassSDK } from './core/sdk';
